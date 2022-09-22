33f5b2fbd201392936f1c6f509abf1976e045234
arditxh0 <ardit@starlabspro.com>
2022-09-22 23:39:57 +0200

    First and probs final commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+from subprocess import Popen, PIPE, STDOUT
+
+from capturing import Capturing
+
+parent = "../5HQ1P/5HQ1P-Interpreter/"
+sys.path.insert(1, parent)
+
+from basic import Core
+core = Core()
+
+from fastapi import FastAPI, Form
+
+app = FastAPI()
+
+@app.post("/compile-sq")
+def hello(name: str = Form()):
+  with Capturing() as output:
+    result, error = core.run('<index.sq>', name)
+  
+  if error:
+    return error.as_string()
+  elif output:
+    return output
+  else:
+      if len(result.elements) == 1:
+          return(repr(result.elements[0]))
+      else:
+          return(repr(result))
